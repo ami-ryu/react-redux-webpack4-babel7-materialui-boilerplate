b43a456bd8da16e367aea6d7f2f4776a06ef5020
Ryu <ryu.director@kakaocorp.com>
2019-03-06 18:34:08 +0900

    apply material-ui paperbase theme

--- src/index.js
+++ src/index.js
@@ -1,18 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Button from '@material-ui/core/Button';
 import "./style.css"
+import Paperbase from "./Paperbase";
 
-function App() {
-    return (
-        <Button variant="contained" color="primary">
-            Hello World
-        </Button>
-    )
-}
-
-const appElement = document.getElementById('root');
-ReactDOM.render(
-    <App />,
-    appElement
-);
+ReactDOM.render(<Paperbase />, document.querySelector("#root"));
